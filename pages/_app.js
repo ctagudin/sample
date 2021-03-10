ff6103e0a6e086995994720574402d1e0859faae
ctagudin <65007512+ctagudin@users.noreply.github.com>
2021-03-10 11:33:20 +0800

    031021

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import "../styles/index.css"
+import "../styles/main.scss"
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
